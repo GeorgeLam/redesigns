22369d26aeaf6720e2d5fa5c7abe9dec8c32b331
George Lam <2940746+GeorgeLam@users.noreply.github.com>
2020-07-09 18:34:05 +0800

    Sections added below hero area

--- July 9 - Flight finder/app.js
+++ July 9 - Flight finder/app.js
@@ -1,3 +1,13 @@
+//Navbar bg color switch on scroll
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $(".fixed-top");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > ($('#main-bg').height() - $nav.height()));
+        $('.navbar-light .navbar-nav').toggleClass('scrolled', $(this).scrollTop() > ($('#main-bg').height() - $nav.height()));
+    });
+});
+
+//Typeahead for airports
 $(document).ready(function(){
     var data = ["London Heathrow [LHR]", "Shanghai Pudong [PVG]", "Tokyo Narita [NRT]", "Hong Kong [HKG]", "Singapore [SIN]", "Paris [CDG]", "Amsterdam [AMS]", "New York JFK [JFK]"];
     $(".lookahead").typeahead({ source:data });
