3b662254600c8eabaaee63b28ab1ed3231cd8eba
George Lam <2940746+GeorgeLam@users.noreply.github.com>
2020-07-10 23:53:04 +0800

    Added app section and bottom link area. Adjusted colours

--- July 9 - Flight finder/app.js
+++ July 9 - Flight finder/app.js
@@ -12,3 +12,24 @@ $(document).ready(function(){
     var data = ["London Heathrow [LHR]", "Shanghai Pudong [PVG]", "Tokyo Narita [NRT]", "Hong Kong [HKG]", "Singapore [SIN]", "Paris [CDG]", "Amsterdam [AMS]", "New York JFK [JFK]"];
     $(".lookahead").typeahead({ source:data });
 });
+
+
+//Price slider
+valueDetect = function () {
+    if ($("#price-select").val() == 10) {
+        $("#price-display").text(`£0 - £1000+`)
+    } else if ($("#price-select").val() == 0) {
+        $("#price-display").text(`<£100`)
+    } else {
+        console.log("L")
+        $("#price-display").text(`£0 - £${$("#price-select").val() * 100}`)
+    }
+}
+
+$("#price-select").on("mouseup", valueDetect)
+$("#price-select").val(10);
+valueDetect();
+
+
+
+
